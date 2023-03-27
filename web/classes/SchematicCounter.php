6c5ebb0147ec088628cac9e093aa93d4a2042284
sd31117 <sd31117@hotmail.com>
2023-03-27 04:05:23 -0500

    implemented a class that counts how many blocks needed of each chest

---
+++ web/classes/SchematicCounter.php
@@ -0,0 +1,51 @@
+<?php
+class SchematicCounter extends SchematicsView
+{
+    public function __construct($id)
+    {
+        $data = $this->turtleGetSchematic($id);
+        $schematic = json_decode($data[0]['objectData'], true);
+
+        $this->countBlocks($data, $schematic);
+    }
+
+    public function countBlocks($data, $schematic)
+    {
+        $blockOneCount = 0;
+        $blockTwoCount = 0;
+        $blockThreeCount = 0;
+        $blockFourCount = 0;
+        $totalBlocks = 0;
+        for ($i = 0; $i < $data[0]['height']; $i++) {
+            for ($j = 0; $j < $data[0]['width']; $j++) {
+                switch ($schematic[$i][$j]) {
+                    case 0:
+                        $blockOneCount++;
+                        break;
+
+                    case 1:
+                        $blockTwoCount++;
+                        break;
+
+                    case 2:
+                        $blockThreeCount++;
+                        break;
+
+                    case 3:
+                        $blockFourCount++;
+                        break;
+
+                    default:
+                        break;
+                }
+                $totalBlocks++;
+            }
+            // echo "<br>";
+        }
+        echo "1st Chest will require: " . $blockOneCount . "<br>";
+        echo "2nd Chest will require: " . $blockTwoCount . "<br>";
+        echo "3rd Chest will require: " . $blockThreeCount . "<br>";
+        echo "4th Chest will require: " . $blockFourCount . "<br>";
+        echo "Total Block Count: $totalBlocks";
+    }
+}
